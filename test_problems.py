8655526569fdb6aa3ebc1511c250cc534b85bff1
josemarialanda <josemaria.landa@gmail.com>
2021-10-17 13:13:24 -0500

    added examples

--- test_problems.py
+++ test_problems.py
@@ -43,6 +43,83 @@ test3_bounds = [(-10,10),(-10,10),(-10,10),(-10,10),(-10,10),(-10,10),(-10,10)]
 # pso.run()
 # print("\nSolution: ", pso.population.best_particle)
 
+# De Jong's function
+# De Jong's function is also known as sphere model. It is continuos, convex and unimodal.
+# the best known solution is f* = 0
+deJong_5 = lambda x1, x2, x3, x4, x5: sum([xi**2 for xi in [x1, x2, x3, x4, x5]])
+deJong_5_bounds  = [(-5.12,5.12),(-5.12,5.12),(-5.12,5.12),(-5.12,5.12),(-5.12,5.12)]
+
+# pso = PSO(deJong_5, restrictions = [], pop_size=100, max_iter=200, bounds=deJong_5_bounds, s=0.1, w=w, c1=c1, c2=c2, verbose=True)
+# pso.run()
+# print("\nSolution: ", pso.population.best_particle)
+
+# Axis parallel hyper-ellipsoid function
+'''
+The axis parallel hyper-ellipsoid is similar to De Jong's function 1. It is also known as the weighted sphere
+model. Again, it is continuos, convex and unimodal.
+'''
+# the best known solution is f* = 0
+def aphe_5(x1,x2,x3,x4,x5):
+    x = [x1,x2,x3,x4,x5]
+    i = [1,2,3,4,5]
+    res = 0
+    for j in range(5):
+        xi = x[j]
+        ii = i[j] 
+        res = res + ii*xi**2
+    return res
+aphe_5_bounds  = [(-5.12,5.12),(-5.12,5.12),(-5.12,5.12),(-5.12,5.12),(-5.12,5.12)]
+
+# pso = PSO(aphe_5, restrictions = [], pop_size=100, max_iter=200, bounds=aphe_5_bounds, s=0.1, w=w, c1=c1, c2=c2, verbose=True)
+# pso.run()
+# print("\nSolution: ", pso.population.best_particle)
+
+# Rastrigin's function 6
+'''
+Rastrigin's function is based on De Jong's function with the addition of cosine
+modulation to produce many local minima. Thus, the test function is highly multimodal.
+However, the location of the minima are regularly distributed.
+'''
+# the best known solution is f* = 0
+rastrigin_5 = lambda x1, x2, x3, x4, x5: 10*5+sum([xi**2-10*np.cos(2*np.pi*xi) for xi in [x1, x2, x3, x4, x5]])
+rastrigin_5_bounds  = [(-5.12,5.12),(-5.12,5.12),(-5.12,5.12),(-5.12,5.12),(-5.12,5.12)]
+
+# pso = PSO(rastrigin_5, restrictions = [], pop_size=500, max_iter=200, bounds=rastrigin_5_bounds, w=w, c1=c1, c2=c2, s=0.1, verbose=True)
+# pso.run()
+# print("\nSolution: ", pso.population.best_particle)
+
+# Schwefel's function
+'''
+Schwefel's function [Sch81] is deceptive in that the global minimum is geometrically distant, over the parameter
+space, from the next best local minima. Therefore, the search algorithms are potentially prone to convergence in
+the wrong direction.
+'''
+# the best known solution is f* = -n*418.9829 (in this case n=5, so  f* = −2094.9145)
+schwefel_5 = lambda x1, x2, x3, x4, x5: sum([-xi*np.sin(np.sqrt(np.abs(xi))) for xi in [x1, x2, x3, x4, x5]])
+schwefel_5_bounds  = [(-500,500),(-500,500),(-500,500),(-500,500),(-500,500)]
+
+# pso = PSO(schwefel_5, restrictions = [], pop_size=100, max_iter=200, bounds=schwefel_5_bounds, w=w, c1=c1, c2=c2, s=0.1, verbose=True)
+# pso.run()
+# print("\nSolution: ", schwefel_5(*pso.population.best_particle.position))
+
+# Ackley's Path function
+# the best known solution is f* = 0
+def ackley_5(x1,x2,x3,x4,x5):
+    a = 20
+    b = 0.2
+    c = 2*np.pi
+    x = [x1,x2,x3,x4,x5]
+    xi_2_sum   = sum([xi**2 for xi in x])
+    xi_cos_sum = sum([np.cos(c*xi) for xi in x])
+    n = len(x)
+    return -a*np.exp(-b*np.sqrt(1/n*xi_2_sum))-np.exp(1/n*xi_cos_sum)+a+np.exp(1)
+ackley_5_bounds  = [(-32.768,32.768),(-32.768,32.768),(-32.768,32.768),(-32.768,32.768),(-32.768,32.768)]
+
+pso = PSO(ackley_5, restrictions = [], pop_size=100, max_iter=200, bounds=ackley_5_bounds, w=w, c1=c1, c2=c2, s=0.1, verbose=True)
+pso.run()
+print("\nSolution: ", schwefel_5(*pso.population.best_particle.position))
+
+
 # g5 defined in [Michalewicz1996]: the best known solution is f* = 5126.4981
 g5        = lambda x1, x2, x3, x4 : 3*x1+0.000001*x1**3+2*x2+0.000002/(3*x2**3)
 g5_r1     = lambda x1, x2, x3, x4 : (-1)*(x4-x3+0.55)
@@ -59,6 +136,8 @@ g5_bounds = [(0,1200),(0,1200),(-0.55,0.55),(-0.55,0.55)]
 # pso = PSO(g5, restrictions = g5_r, pop_size=100, max_iter=200, bounds = g5_bounds, s=0.1, w=w, c1=c1, c2=c2, verbose=True)
 # pso.run()
 # print(g5(*pso.population.best_particle.position))
+
+# more examples at: http://www.geatbx.com/download/GEATbx_ObjFunExpl_v38.pdf
 
 # experiments (run with s=0.1, s=10 and try pop_size = 100, 500, 1000 and finally try num_experiments = 100 and 1000 with pop_size=100 and s=0.1)
 # i.e try:
@@ -141,3 +220,4 @@ def run_g7_experiments(num_experiments = 10, s=0.1, pop_size=100):
 # experiment_6 = run_g7_experiments(s=10,  pop_size=1000)
 # experiment_7 = run_g7_experiments(num_experiments=100, s=0.1, pop_size=100)
 # experiment_8 = run_g7_experiments(num_experiments=1000, s=0.1, pop_size=100)
+
